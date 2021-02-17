8f1c29e0d28feabe657c4db4613424a128e50c22
William Henderson <william-henderson@outlook.com>
2021-02-17 13:51:00 +0000

    Updated projects and mobile experience

--- script.js
+++ script.js
@@ -8,6 +8,17 @@ function openGitHub(projectName = "") {
   window.open(`https://github.com/w-henderson/${projectName}`);
 }
 
+function scrollToAnchor(name) {
+  document.querySelector("nav").className = "";
+  document.querySelector(`a[name="${name}"]`).scrollIntoView({ behavior: "smooth" });
+  window.history.replaceState(null, null, `#${name}`);
+}
+
+function toggleMenu() {
+  let menuActive = document.querySelector("nav").className === "shown";
+  document.querySelector("nav").className = menuActive ? "" : "shown";
+}
+
 window.addEventListener('scroll', function (e) {
   document.documentElement.dataset.scroll = window.pageYOffset;
 });
